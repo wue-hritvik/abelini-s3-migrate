8bde6193cdfe6619a15a91501f0f455e3d5159a5
Hritvik Soni <hritviksoni768@gmail.com>
2025-06-30 15:48:30 +0530

    refactor code of image migrate only failed batch and refactor code for file name compare and file name export

--- src/main/java/com/abelini_s3_migrate/service/ShopifyFileFetcherService.java
+++ src/main/java/com/abelini_s3_migrate/service/ShopifyFileFetcherService.java
@@ -97,7 +97,7 @@ public class ShopifyFileFetcherService {
 
             String query = """
                     {
-                      files(first: 250, query: "created_at:>=2025-06-29"%s) {
+                      files(first: 250, query: "created_at:>=2025-06-27"%s) {
                         edges {
                           node {
                             alt
